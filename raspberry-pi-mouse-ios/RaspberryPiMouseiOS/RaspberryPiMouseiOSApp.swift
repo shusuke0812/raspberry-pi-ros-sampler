4392db81fa3881e0d5b5ee6b03c961a1e18ed5ac
shusuke0812 <shu.ota0812@gmail.com>
2025-10-13 19:36:23 +0900

    iOSプロジェクトを追加

---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/RaspberryPiMouseiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaspberryPiMouseiOSApp.swift
+//  RaspberryPiMouseiOS
+//
+//  Created by Shusuke Ota on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct RaspberryPiMouseiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOSTests/RaspberryPiMouseiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RaspberryPiMouseiOSTests.swift
+//  RaspberryPiMouseiOSTests
+//
+//  Created by Shusuke Ota on 2025/10/13.
+//
+
+import Testing
+@testable import RaspberryPiMouseiOS
+
+struct RaspberryPiMouseiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
