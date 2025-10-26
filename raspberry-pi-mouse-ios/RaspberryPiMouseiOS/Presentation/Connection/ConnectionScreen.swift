1820429cb90a0a879be62d785afbef16be4baf89
shusuke0812 <shu.ota0812@gmail.com>
2025-10-26 11:05:26 +0900

    ConnectionScreen、TopicScreenを追加

---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/Presentation/Connection/ConnectionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionScreen.swift
+//  RaspberryPiMouseiOS
+//
+//  Created by Shusuke Ota on 2025/10/26.
+//
+
+import SwiftUI
+
+struct ConnectionScreen: View {
+    var body: some View {
+        Text("Connection")
+    }
+}
+
+#Preview {
+    ConnectionScreen()
+}
