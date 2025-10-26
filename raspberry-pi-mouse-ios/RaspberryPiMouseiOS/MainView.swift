ea2569e90767f1fa5b34cf0c3210191a58714bdc
shusuke0812 <shu.ota0812@gmail.com>
2025-10-26 10:10:01 +0900

    ContentViewからMainViewに名称を変更

--- raspberry-pi-mouse-ios/RaspberryPiMouseiOS/MainView.swift
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  RaspberryPiMouseiOS
 //
 //  Created by Shusuke Ota on 2025/10/13.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
