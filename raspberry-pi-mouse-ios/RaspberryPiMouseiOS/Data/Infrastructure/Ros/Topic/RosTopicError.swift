ab217090a7ce13d6d872deee580836751be3573a
shusuke0812 <shu.ota0812@gmail.com>
2025-11-03 10:22:27 +0900

    HelloResponse型、RosTopicError型を追加

---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/Data/Infrastructure/Ros/Topic/RosTopicError.swift
@@ -0,0 +1,12 @@
+//
+//  RosTopicError.swift
+//  RaspberryPiMouseiOS
+//
+//  Created by Shusuke Ota on 2025/11/3.
+//
+
+import Foundation
+
+enum RosTopicError: Error {
+    case failedDecodeMessageToRosPublish
+}
---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/Data/Repository/Hello/HelloResponse.swift
@@ -0,0 +1,10 @@
+//
+//  HelloResponse.swift
+//  RaspberryPiMouseiOS
+//
+//  Created by Shusuke Ota on 2025/11/3.
+//
+
+import Foundation
+
+typealias HelloResponse = RosTopicPublish<StringMessage>
