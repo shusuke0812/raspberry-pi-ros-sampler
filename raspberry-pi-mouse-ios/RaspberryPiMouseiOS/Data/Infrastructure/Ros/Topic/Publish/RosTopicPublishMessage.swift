36bf754d72fa4692a30c5a34b336cfd91de46a31
shusuke0812 <shu.ota0812@gmail.com>
2025-11-09 22:17:40 +0900

    RosTopicPublishをHeader部とMessage部に分割した

---
+++ raspberry-pi-mouse-ios/RaspberryPiMouseiOS/Data/Infrastructure/Ros/Topic/Publish/RosTopicPublishMessage.swift
@@ -0,0 +1,20 @@
+//
+//  RosTopicPublishMessage.swift
+//  RaspberryPiMouseiOS
+//
+//  Created by Shusuke Ota on 2025/11/9.
+//
+
+import Foundation
+
+struct RosTopicPublishMessage<T: RosMessageProtocol>: Codable {
+    let message: T
+
+    enum CodingKeys: String, CodingKey {
+        case message = "msg"
+    }
+
+    init(message: T) {
+        self.message = message
+    }
+}
