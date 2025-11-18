c808875a6c3d1543213ba1fdea2b69d7698dce27
shusuke0812 <shu.ota0812@gmail.com>
2025-11-18 23:17:12 +0900

    WebScoketレスポンスで取得できるメッセージの種類を判別する型を追加

--- raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/Ros/RosBridgeMessageOperation.swift
+++ raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/Ros/RosBridgeMessageOperation.swift
@@ -14,3 +14,29 @@ enum RosBridgeMessageOperation: String, Codable {
     case callService = "call_service"
     case serviceResponse = "service_response"
 }
+
+/// RosBridgetClientのメッセージレスポンスの種別を判別するために使用する.
+/// 判別した後、`publish`, `serviceResponse`毎にデコード処理を行う.
+enum HandleRosBridgeMessageOperation {
+    case publish
+    case serviceResponse
+
+    init?(_ op: RosBridgeMessageOperation) {
+        switch op {
+        case .publish:
+            self = .publish
+        case .serviceResponse:
+            self = .serviceResponse
+        default:
+            return nil
+        }
+    }
+}
+
+struct HandleRosBridgeMessageResponse: Codable {
+    var op: RosBridgeMessageOperation
+
+    enum CodingKeys: String, CodingKey {
+        case op
+    }
+}
