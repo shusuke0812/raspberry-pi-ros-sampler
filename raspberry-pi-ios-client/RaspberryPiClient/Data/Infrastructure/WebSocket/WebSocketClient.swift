f048cdd256fd7dbbe801e5952914d8149beb022f
shusuke0812 <shu.ota0812@gmail.com>
2025-11-18 23:16:47 +0900

    コメントを追加

--- raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/WebSocket/WebSocketClient.swift
+++ raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/WebSocket/WebSocketClient.swift
@@ -79,6 +79,7 @@ final class WebSocketClient: NSObject {
                 }
             }
         } catch {
+            // Network errors, WebSocket connection errors
             messageContinuation?.finish(throwing: error)
         }
     }
