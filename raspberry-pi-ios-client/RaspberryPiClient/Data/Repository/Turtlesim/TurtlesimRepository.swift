bc7304fe2e6d502a84e4301dc798320f0ad32599
shusuke0812 <shu.ota0812@gmail.com>
2025-12-28 20:14:07 +0900

    Turtlesimのドキュメントリンクを追加

--- raspberry-pi-ios-client/RaspberryPiClient/Data/Repository/Turtlesim/TurtlesimRepository.swift
+++ raspberry-pi-ios-client/RaspberryPiClient/Data/Repository/Turtlesim/TurtlesimRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// [ROS Turtlesim Doc](https://wiki.ros.org/turtlesim)
 protocol TurtlesimRepositoryProtocol {
     func callSpawn(x: Float, y: Float, theta: Float, onMessage: @escaping (Result<Void, RosServiceError>) -> Void)
 }
@@ -18,6 +19,7 @@ class TurtlesimRepository: TurtlesimRepositoryProtocol {
         self.rosBridgeClient = rosBridgeClient
     }
 
+    /// [Spawn Request](https://docs.ros.org/en/api/turtlesim/html/srv/Spawn.html)
     func callSpawn(x: Float, y: Float, theta: Float, onMessage: @escaping (Result<Void, RosServiceError>) -> Void) {
         let arg = TurtlesimServiceArgs(x: x, y: y, theta: theta, name: nil)
         let callService = RosCallService<TurtlesimServiceArgs, TurtlesimServiceResponse>(
