89df0c1b76f3d40351210802c5bc098e266af892
shusuke0812 <shu.ota0812@gmail.com>
2025-12-28 14:02:27 +0900

    RosServiceErrorにdescriptionプロパティを追加

--- raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/Ros/Service/RosServiceError.swift
+++ raspberry-pi-ios-client/RaspberryPiClient/Data/Infrastructure/Ros/Service/RosServiceError.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 enum RosServiceError: Error {
-    case alreadyCalling
+    case alreadyCalling // TODO: 不要？
     case failedReceiveMessage(reason: Error)
     case failedDecodeMessage(reason: Error)
+
+    var description: String {
+        switch self {
+        case .alreadyCalling:
+            return ""
+        case .failedReceiveMessage(let reason):
+            return "サービスの送信に失敗しました\n\(reason.localizedDescription)"
+        case .failedDecodeMessage(let reason):
+            return "レスポンスの変換に失敗しました\(reason.localizedDescription)"
+        }
+    }
 }
