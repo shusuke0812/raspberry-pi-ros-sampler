2fe2946946310c475015ac0c37b3aa2f30f34bbd
shusuke0812 <shu.ota0812@gmail.com>
2025-12-29 22:40:03 +0900

    TurtlesimRepositoryにmoveTurtle()を追加

--- raspberry-pi-ios-client/RaspberryPiClient/Data/Repository/Turtlesim/TurtlesimRepository.swift
+++ raspberry-pi-ios-client/RaspberryPiClient/Data/Repository/Turtlesim/TurtlesimRepository.swift
@@ -10,10 +10,12 @@ import Foundation
 /// [ROS Turtlesim Doc](https://wiki.ros.org/turtlesim)
 protocol TurtlesimRepositoryProtocol {
     func spawnTurtle(x: Float, y: Float, theta: Float, onMessage: @escaping (Result<Void, RosServiceError>) -> Void)
+    func moveTurtle(x: Float, y: Float, theta: Float)
 }
 
 class TurtlesimRepository: TurtlesimRepositoryProtocol {
     private let rosBridgeClient: RosBridgeMessageProtocol
+    private let turtleName = "turtle2"
 
     init(rosBridgeClient: RosBridgeMessageProtocol = RosBridgeClient.shared) {
         self.rosBridgeClient = rosBridgeClient
@@ -21,7 +23,7 @@ class TurtlesimRepository: TurtlesimRepositoryProtocol {
 
     /// [Spawn Request](https://docs.ros.org/en/api/turtlesim/html/srv/Spawn.html)
     func spawnTurtle(x: Float, y: Float, theta: Float, onMessage: @escaping (Result<Void, RosServiceError>) -> Void) {
-        let arg = TurtlesimServiceArgs(x: x, y: y, theta: theta, name: nil)
+        let arg = TurtlesimServiceArgs(x: x, y: y, theta: theta, name: turtleName)
         let callService = RosCallService<TurtlesimServiceArgs, TurtlesimServiceResponse>(
             service: "/spawn",
             arg: arg
@@ -34,5 +36,17 @@ class TurtlesimRepository: TurtlesimRepositoryProtocol {
                 onMessage(.failure(error))
             }
         }
+    }
+
+    func moveTurtle(x: Float, y: Float, theta: Float) {
+        let message = TwistMessage(
+            linear: Vector3Message(x: Double(x), y: Double(y), z: 0),
+            angular: Vector3Message(x: 0, y: 0, z: Double(theta))
+        )
+        let topic = RosTopicPublish<TwistMessage>(
+            topic: "/\(turtleName)/cmd_vel",
+            message: message
+        )
+        rosBridgeClient.publish(topic: topic)
     }
 }
