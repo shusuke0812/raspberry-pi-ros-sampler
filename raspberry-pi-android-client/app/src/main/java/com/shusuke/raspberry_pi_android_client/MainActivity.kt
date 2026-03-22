de85af2373e5fac73db0c7e102e83ffede6a8592
shusuke0812 <shu.ota0812@gmail.com>
2026-03-22 23:28:11 +0900

    Androidプロジェクトを追加

---
+++ raspberry-pi-android-client/app/src/main/java/com/shusuke/raspberry_pi_android_client/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.shusuke.raspberry_pi_android_client
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shusuke.raspberry_pi_android_client.ui.theme.RaspberrypiandroidclientTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RaspberrypiandroidclientTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RaspberrypiandroidclientTheme {
+        Greeting("Android")
+    }
+}
